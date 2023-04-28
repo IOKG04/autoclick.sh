214e9a193bdd840fb2dfcced68a309d3620bc619
IOKG04 <iokg04@gmail.com>
2023-04-28 23:19:51 +0200

    Removed time spent waiting

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 # autoclick.sh
-Simple auto-clicker shell script I made for a friend and I.
+Simple auto-clicker shell script I forked and removed the time spent waiting from.
 
-Tested on Ubuntu and Arch, but theoretically should work on any Debian or Arch based distro.
-
-Use the "[releases](https://github.com/CyanJDroid/autoclick.sh/releases)" tab above to see and download the latest version.
+Tested on Arch, but theoretically should work on any distro.
--- autoclick.sh
+++ autoclick.sh
@@ -5,21 +5,16 @@ GREEN='\033[1;32m'
 WHITE='\033[1;37m'
 NC='\033[0m' # No Color
 
-clear
 echo "-- autoclick.sh - ver. 2.2.0 --"
 echo " "
-sleep 1
-echo This script is designed to be run on a Debian or Arch based system.
-sleep 1
+echo This specific version of this script was only tested on Arch, should work on other things too though.
 echo Ensuring xdotool is installed...
-sleep 1
 
 #check if pacman is installed, if it is use it to check if xdotool is instaled and install it if it isn't
 #if command -v pacman >/dev/null; then
 if which pacman >/dev/null; then
     if ! pacman -Qq | grep -qw xdotool; then
         echo -e "${RED}xdotool not found, installing, this may ask for your password...${NC}"
-        sleep 2
         sudo pacman -Sy xdotool
     fi
     #check if apt is installed, if it is use it to check if xdotool is instaled and install it if it isn't
@@ -27,39 +22,25 @@ if which pacman >/dev/null; then
     elif which apt >/dev/null; then
     if ! dpkg -l | grep -qw xdotool; then
         echo -e "${RED}xdotool not found, installing, this may ask for your password...${NC}"
-        sleep 2
         sudo apt-get update
         sudo apt-get install xdotool
     fi
     #if both pacman and apt couldn't be found, report error
 else
     echo pacman or apt not found! exiting...
-    sleep 5
     exit 1
 fi
 
-sleep 2
-clear
-echo "-- autoclick.sh - ver. 2.2.0 --"
-echo " "
 echo "What mouse button would you like pressed?"
 echo "1 (left), 2 (middle), or 3 (right). "
 read mouse
-sleep 1
-clear
-echo "-- autoclick.sh - ver. 2.2.0 --"
-echo " "
-echo "How long between each mouse press (in seconds)? "
+echo "What should be the delay between button clicks? (In seconds)"
 read clock
 
-sleep 1
-clear
-echo "-- autoclick.sh - ver. 2.2.0 --"
-echo " "
-echo -e "${GREEN}This script will begin in 5 seconds with these settings:${NC}"
+echo -e "${GREEN}This script will begin in 1 seconds with these settings:${NC}"
 echo "Mouse: $mouse  Delay: $clock"
 echo -e "Use ${WHITE}[Ctrl-C]${NC} in this terminal to quit."
-sleep 5
+sleep 1
 
 # The code below is what does the actual clicking.
 
